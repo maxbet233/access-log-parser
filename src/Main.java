a112b8741f13b3a439f0b9c77f3b970c5e8b2e84
maxbet233 <maks_233@list.ru>
2024-11-24 12:37:40 +0300

    получение чисел из консоли и выполнение операций с ними

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число:");
+        int firstNumber = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
+        int secondNumber = new Scanner(System.in).nextInt();
+
+        System.out.println("Сумма чисел равна "  + (firstNumber + secondNumber));
+        System.out.println("Разность чисел равна "  + (firstNumber - secondNumber));
+        System.out.println("Произведение чисел равно "  + (firstNumber * secondNumber));
+        System.out.println("Частное чисел равно "  + (double) firstNumber / secondNumber);
     }
 }
