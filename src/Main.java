fb72da9a2ee5f93ad4669d9cd546ec61230bb30d
maxbet233 <maks_233@list.ru>
2024-12-05 09:22:29 +0300

    добавлен бесконечный цикл проверок пути к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        int count = 1;
+        while (true) {
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+
+            boolean fileExist = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExist || isDirectory) {
+                System.out.println("Файл не найден, введите полный путь");
+                continue;
+            }
+            System.out.println("Путь указан верно! Это файл номер " + count);
+            count++;
+        }
     }
 }
