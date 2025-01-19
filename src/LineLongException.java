e55639046821976f623f5653903d049cf212b36b
maxbet233 <maks_233@list.ru>
2025-01-19 14:44:01 +0300

    добавлен класс с исключением, подсчет количества строк, длина самой длинной и короткой строк

---
+++ src/LineLongException.java
@@ -0,0 +1,6 @@
+public class LineLongException extends Exception{
+
+    public LineLongException(String message) {
+        super(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
         int count = 1;
         while (true) {
+            System.out.println("Введите путь до файла:");
             String path = new Scanner(System.in).nextLine();
             File file = new File(path);
 
@@ -17,6 +19,35 @@ public class Main {
             }
             System.out.println("Путь указан верно! Это файл номер " + count);
             count++;
+
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader = new BufferedReader(fileReader);
+                String line;
+
+                int countLine = 0;
+                int minLine = Integer.MAX_VALUE;
+                int maxLine = 0;
+
+                while ((line = reader.readLine()) != null) {
+                    int length = line.length();
+                    if (length > 1024) throw new LineLongException("Длина строки превышает 1024 символа");
+                    if (length > maxLine) {
+                        maxLine = length;
+                    }
+                    if (length < minLine) {
+                        minLine = length;
+                    }
+                    countLine++;
+                }
+                System.out.println(countLine);
+                System.out.println(maxLine);
+                System.out.println(minLine);
+
+            } catch (LineLongException | IOException ex) {
+                ex.printStackTrace();
+            }
+
         }
     }
 }
